eb65e4745bb4ee89e2b06419d7d765c59e2f6329
sovlookup <gonorth@qq.com>
2022-01-24 22:51:58 +0800

    gb

--- src/views/ModelBuilder.vue
+++ src/views/ModelBuilder.vue
@@ -1,14 +1,20 @@
 <template>
-  <Container ref="childRef" />
-  <div class="toolbar d-flex justify-center align-center">
-    <v-btn class="mx-1" icon @click="fd"> <v-icon icon="mdi-plus" /></v-btn>
-    <v-btn class="mx-1" icon @click="sx"> <v-icon icon="mdi-minus" /></v-btn>
-    <v-btn class="mx-1" icon @click="rf">
-      <v-icon icon="mdi-scan-helper"
-    /></v-btn>
-    <v-btn class="mx-1" icon @click="sv">
-      <v-icon icon="mdi-content-save"
-    /></v-btn>
+  <div class="container">
+    <Container ref="childRef" />
+  </div>
+  <div class="toolbar" align="center">
+    <q-btn class="q-pa-md q-mx-xs" round @click="fd">
+      <Icon icon="mdi-plus"
+    /></q-btn>
+    <q-btn class="q-pa-md q-mx-xs" round @click="sx">
+      <Icon icon="mdi-minus"
+    /></q-btn>
+    <q-btn class="q-pa-md q-mx-xs" round @click="rf">
+      <Icon icon="mdi-scan-helper"
+    /></q-btn>
+    <q-btn class="q-pa-md q-mx-xs" round @click="sv">
+      <Icon icon="mdi-content-save"
+    /></q-btn>
   </div>
 </template>
 
@@ -16,6 +22,7 @@
 import Container from '@/components/GraphCanvas.vue'
 import { notify } from '@kyvg/vue3-notification'
 import { onBeforeMount, onMounted, ref } from 'vue'
+import { Icon } from '@iconify/vue'
 
 const childRef = ref('childRef')
 let c: any
@@ -60,5 +67,13 @@ const sv = async () => {
   border-radius: 10px;
   border: 1px solid rgba(175, 175, 175, 0.767);
   background-color: rgb(243, 243, 243);
+}
+
+.container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
